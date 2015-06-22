831d50c8214880410ab8f0267718e94939fe2d35
j3rome <jerome.abdelnour@usherbrooke.ca>
2015-06-21 22:30:39 -0400

    + Can now parse and reassemble the calendars

--- main.js
+++ main.js
@@ -12,29 +12,35 @@ var fs = require("fs");
     // TODO : Enleve tutorat inutile (Faire le check a la creation de l'objet JSON ? ou apres?)
     // TODO : enleve multi-day events
     // TODO : Polling 3-4 fois par jour durant heures ouvrables (dison entre 5h du matin & 11h le soir) pour voir si ya du changement dans le ical (Garder une copie de la string recu pour comparaison rapide ?)
-    // TODO : Reconstruire ical
     // TODO : Http server pour repondre a la requete (On devrais effacer les events inutile a ce moment puisque c'est la qu'on recoit les parametres
     // TODO : Handle les infos du calendrier
     // TODO : S'assurer de bien catcher les exceptions, le service ne doit pas crasher
 
 request.get("http://www.gel.usherbrooke.ca/horarius/ical?cip=abdj2702", function(error, response, body){
-if (!error) {
-    
-    var parsedBody = parseResponse(body);
-    //parsedBody.calendarInfos
-    
-    var eventList = parseEvents(parsedBody.events);
-    writeEvents(eventList);
-    
-    //console.log(eventList[50]["LOCATION"]);
-}else{
-    console.log(error);
-}
+    if (!error) {
+        // FOR TESTING : Save the input so we can compare it later
+        fs.writeFile("test.input", body, function(err){
+            if(err){
+                console.log(err);
+            }else{
+                console.log("The input was saved to test.input");
+            }
+        });
+        
+        var parsedBody = parseResponse(body);       //Parse the body of the response
+        
+        var eventList = parseEvents(parsedBody.events);
+        
+        writeCalendar(parsedBody.calendarInfos, eventList);
+
+    }else{
+        console.log(error);
+    }
 });
 
 // TODO : Separate in helper file ?
-
 function parseResponse(body){
+    // TODO : Add some verification that we got the right response body
     var calendarInfos = body.substring(0,body.indexOf("BEGIN:VEVENT"))
     var events = body.substring(body.indexOf("\r\nBEGIN:VEVENT"),body.length);
     
@@ -45,37 +51,57 @@ function parseEvents(events){
     var json = {};
     var eventLines;
     var keyAndValue;
+    var splitIndex;
     
     for(var j=0;j<events.length;j++){
         eventLines = events[j].split("\r\n");
-        console.log("Event "+j);
         for(var i=0;i<eventLines.length;i++){
-            console.log("Event Line : "+i);
-            keyAndValue = eventLines[i].split(":");
-            if(keyAndValue[0] != "END" && keyAndValue[0] != "" && keyAndValue[0] != undefined && keyAndValue[1] != undefined){            // We don't want the end line into the parsed object
+            splitIndex = eventLines[i].indexOf(':');
+            
+            keyAndValue = [eventLines[i].substring(0,splitIndex),eventLines[i].substring(splitIndex+1)];
+            
+            // We don't want the END object into the parsed object
+            if(keyAndValue[0] != "END" && keyAndValue[0] != "" && keyAndValue[0] != undefined && keyAndValue[1] != undefined){            
                 json[keyAndValue[0]] = keyAndValue[1];
             }
         }
-        console.log(json);
         if(Object.keys(json).length > 0){
             eventList.push(json);
-            console.log("PUSHED !");
+            json = {};
         }
-        json = {};
     }
     
     return eventList;
 }
 
-function writeEvents(eventList){
+function writeCalendar(calendarInfos, eventList){
     // TODO : Use async writing ?
-    var fileContent = "";
-    fileContent = JSON.stringify(eventList);
-    fs.writeFile("test.output", fileContent, function(err){
+    var keys,
+        key,
+        fileContent = "";
+        
+    console.log("Writing Calendar to file.");
+    
+    // TODO : Parse the calendarInfos and rewrite them
+    fileContent += calendarInfos;               // We add the calendar header
+    
+    for(var i=0; i<eventList.length;i++){
+        keys = Object.keys(eventList[i]);
+        fileContent += "BEGIN:VEVENT\r\n";      // We add the delimiter for a new event
+        for(var j=0; j< keys.length;j++){
+            key = keys[j];
+            fileContent += key+":"+eventList[i][key]+"\r\n";
+        }
+        fileContent += "END:VEVENT\r\n";        // We add the delimiter for the end of an event
+    }
+    
+    fileContent += "END:VCALENDAR\r\n";         // We add the delimiter for the end of the calendar
+    
+    fs.writeFile("test.output", fileContent, function(err){     // Writing to file          // TODO : Send string as payload to HTTP request response
         if(err){
             console.log(err);
         }else{
-            console.log("The file was saved!");
+            console.log("The calendar was saved to test.output");
         }
     });
 }
